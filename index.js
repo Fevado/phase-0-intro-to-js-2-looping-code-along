a82d62066844c496b4a45b7685097a6475f2e923
favoured mwange <mwangefavoured@gmail.com>
2024-07-12 10:07:27 +0300

    practices looping

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(names,event){
+    let supMessage = []
+      for (let l=0; l<names.length; l++){
+        let message= `Thank you, ${names[l]}, for the wonderful ${event} gift!`
+        supMessage.push(message)
+      }return supMessage
+}
+
+
+function countDown(number){
+    //let number = 10;
+while (number >=0) {
+  console.log(number --);
+}
+ }
